401def1b9308632ca401fc30374c86385b6ea939
dz2244 <dz2244@bs.amalnet.k12.il>
2025-03-07 17:12:28 +0200

    commit

--- app/src/main/java/com/example/sharedfile/MainActivity.java
+++ app/src/main/java/com/example/sharedfile/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.sharedfile;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -7,13 +8,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    String name ;
-    int counter ;
+    String name;
+    int counter;
     TextView tvCounter;
-    Button btnCounter,btnReset,btnExit;
+    Button btnCounter, btnReset, btnExit;
     EditText etName;
 
+    /**
+     * Called when the activity is first created.
+     * Initializes UI elements and retrieves stored preferences.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down, this Bundle contains the most recent data.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,32 +34,44 @@ public class MainActivity extends AppCompatActivity {
         btnExit = findViewById(R.id.btnExit);
         etName = findViewById(R.id.etName);
 
-        SharedPreferences settings =getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
-        SharedPreferences.Editor editor= settings.edit();
-        counter = settings.getInt("counter",-100);
-        tvCounter.setText (counter+"");
-        name = settings.getString("name","notWorking");
+        SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
+        counter = settings.getInt("counter", -100);
+        tvCounter.setText(counter + "");
+        name = settings.getString("name", "notWorking");
         etName.setText(name);
-
     }
 
+    /**
+     * Increments the counter value and updates the displayed text.
+     *
+     * @param view The button view that was clicked.
+     */
     public void clickedBtnCount(View view) {
         counter++;
-        tvCounter.setText(counter+"");
-
+        tvCounter.setText(counter + "");
     }
 
+    /**
+     * Resets the counter to zero and updates the displayed text.
+     *
+     * @param view The button view that was clicked.
+     */
     public void clickedBtnReset(View view) {
-        counter = 0 ;
-        tvCounter.setText(counter+"");
+        counter = 0;
+        tvCounter.setText(counter + "");
     }
 
+    /**
+     * Saves the current counter value and name to SharedPreferences and exits the application.
+     *
+     * @param view The button view that was clicked.
+     */
     public void clickedBtnExit(View view) {
-        SharedPreferences settings =getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
-        SharedPreferences.Editor editor= settings.edit();
-        editor.putInt("counter",counter);
+        SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("counter", counter);
         name = etName.getText().toString();
-        editor.putString("name",name);
+        editor.putString("name", name);
         editor.commit();
         finish();
     }
